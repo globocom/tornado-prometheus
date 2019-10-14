293b7402f08a81829ddf91467989ac6782c75ee1
Wilson Júnior <wilsonpjunior@gmail.com>
2019-10-14 12:00:32 -0300

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tornado-prometheus',
+    version='0.1.0',
+    description="HTTP metrics for a tornado application",
+    long_description=open('README.rst').read(),
+    keywords='prometheus tornado',
+    author=u'Globo.com',
+    author_email='backstage@corp.globo.com',
+    url='https://github.com/globocom/tornado-prometheus',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=find_packages(
+        exclude=(
+            'tests',
+        ),
+    ),
+    include_package_data=True,
+    install_requires=[
+        'tornado>=4.0',
+    ],
+)
